1668168851a1c8f8891756790c28180757cd7207
mozamjs <mozamqurexhi38@gmail.com>
2026-08-01 21:04:17 +0500

    python start

---
+++ apna_collage_python_Basic/Leacture1_variable_dataTypes/1st_program.py
@@ -0,0 +1,77 @@
+# print("Hello world")
+# print("my name is mozam")
+# print("my age is 20")
+# print("i am a webdeveloper", "Cs student")
+
+# print(23)
+# print(35+23)
+
+# name = "shradha"
+# age = 23
+# price = 25.99
+
+
+
+# print("her name is :", name ,"And her age is: ", age ,"And she buy a show of: $",price)
+
+# print(type(name))
+# print(type(age))
+# print(type(price))
+
+# Boolean
+
+# old =False
+# a = None
+
+# print(type(old))
+# print(type(a))
+
+
+# a = 200
+# b = 520
+# sum = a + b
+# print(sum)
+
+# a =  5
+# b = 2
+# mod = a % b
+# print(mod)
+# print(a + b)
+# print(a * b)
+# print(a / b)
+# print(a % b)
+# print(a ** b)
+
+# type conversion  auto convert the type of variable
+# a = 2
+# b = 4.25
+
+# sum = a + b # 2.0 + 4.25 => 6.25
+# print(sum)
+
+# a = "2"
+# b = 2.30
+# print(a+b) #type not convertnig from string to float
+
+
+# type casting  forcly define to convert in this type 
+
+# a = int("2")
+# b = 2.30
+
+# print(type(a))
+
+# print(a+b)
+
+# a = 3.14
+# a = str(a)
+# print(type(a))
+
+
+# inputs in python (user input)
+# a = input("Enter your name:") #alwas as a string by default 
+# print("welcom",a)
+
+name = int(input("Enter your age"))
+print(type(name), name)
+
---
+++ apna_collage_python_Basic/Leacture1_variable_dataTypes/tempCodeRunnerFile.py
@@ -0,0 +1,13 @@
+# print("Hello world")
+# print("my name is mozam")
+# print("my age is 20")
+# print("i am a webdeveloper", "Cs student")
+
+# print(23)
+# print(35+23)
+
+name = "shradha"
+age = 23
+price = 25.99
+
+print(name)
