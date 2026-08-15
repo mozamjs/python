bc23669f1cbc8963dbe0faa01c6208031bcbd7ca
mozamjs <mozamqurexhi38@gmail.com>
2026-08-16 02:57:57 +0500

    data visualization done

---
+++ 4-Data Visualization/Matplotlib/01_basic_plot.py
@@ -0,0 +1,26 @@
+# import matplotlib 
+# print(matplotlib.__version__)
+
+# import matplotlib.pyplot as plt
+
+# x = [1,2,3,4,5]
+# y = [10, 20, 15, 30, 25]
+
+# fruit = ['Apple', "Banana", 'Mango', 'orange', 'Grapes' ]
+# Sales = [50, 80, 60, 40, 70]
+
+# plt.bar(fruit, Sales )
+# plt.pie(Sales, labels = fruit)
+
+
+# plt.show()
+
+
+
+import matplotlib.pyplot as plt
+
+x = [1,2,3,4,5]
+y = [10,20,30,40,50]
+
+plt.plot(x,y)
+plt.show()
